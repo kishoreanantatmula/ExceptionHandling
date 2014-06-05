f8649609246fccbca27f8e0cb4cf8081902f01d7
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 16:49:46 +0530

    commit
    Commit

---
+++ Exceptions/src/demo.java
@@ -0,0 +1,24 @@
+
+public class demo {
+
+	
+	public static void main(String args[])
+	{
+				
+		try
+		{
+			System.out.println("Is it Automatically chnaging in GIt Hub");
+			System.out.println("Did I Chnage Something");
+			System.out.println(10/0);
+		}
+		
+		catch(Exception e)
+		{
+			
+			//e.printStackTrace();
+			
+		}
+		
+	}
+	
+}
